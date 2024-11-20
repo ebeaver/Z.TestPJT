f6c4190d9e0e1778dee734f921cc47ba6f7d6dce
bitcamp <bitcamp@DESKTOP-FAGCDN3>
2024-11-20 11:40:52 +0900

    Create : 2024.10.20
    ~~~~

---
+++ src/product/B.java
@@ -0,0 +1,9 @@
+package product;
+
+public class B {
+
+	public B() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/user/A.java
@@ -0,0 +1,9 @@
+package user;
+
+public class A {
+
+	public A() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
